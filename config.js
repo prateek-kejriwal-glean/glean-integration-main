5080864a88875f90eafafea22aeabc9d13fd8030
Prateek Kejriwal <prateek.kejriwal@glean.com>
2025-04-12 20:10:42 +0530

    added support for OKTA based client APIs. Moved credentials to a separate file. removed stock credentials from front end.

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
+const creds = require('./creds')
 module.exports = {
-    indexingToken: '',
-    clientToken:'--',
-    globalToken:process.env.globalToken,
+    indexingToken: '--',
+    clientToken: '--',
+    globalToken: '--',
+    idpClientId: creds.idpClientId,
+    idpClientSecret: creds.idpSecret,
+    idpHost: creds.idpHost,
+    idpRedirectUri: creds.idpRedirectUri,
     dataSourceId: 'prateektest',
     host: 'https://support-lab-be.glean.com',
-    docCSV:'./input/documents.csv',
-    loggingPath:'./logs'
+    docCSV: './input/documents.csv',
+    loggingPath: './logs'
 }
--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const config = require('./config')
+const cors = require('cors')
+const KeyV = require('keyv').Keyv
+const { KeyvFile } = require('keyv-file')
+
+globalThis.cache = new KeyV({
+    store: new KeyvFile({
+        filename: './cache/auth.cache',
+        encode: JSON.stringify,
+        decode: JSON.parse
+    })
+})
+
 const { log, init: initLogger } = require('./lib/logger/index')
-initLogger({ loggingPath: config.loggingPath, fileName: 'demo-logs', toConsole: false })
+initLogger({ loggingPath: config.loggingPath, fileName: 'logs.log', toConsole: true })
 globalThis.log = log
 
 const GleanApis = require('./lib/api')
 const api = new GleanApis(config.host, config.clientToken, config.globalToken)
 const gleanClient = api.getClient()
 globalThis.gleanClient = gleanClient
+globalThis.config = config
+
 
 
 
 
 const Express = require('express')
 const app = Express()
-const {extractAuthTokenToBody} = require('./lib/middlewares/auth-token-extract')
+const { extractAuthTokenToBody } = require('./lib/middlewares/auth-token-extract')
+const { extractAPIHost } = require('./lib/middlewares/extract-api-host')
+const { extractAuthStateToRequest } = require('./lib/middlewares/auth-state-extract')
+const cookieParser = require('cookie-parser')
 
 
 
-app.use(extractAuthTokenToBody)
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cookieParser(), extractAuthStateToRequest, extractAuthTokenToBody, extractAPIHost)
 app.use(Express.json())
 app.use('/api', require('./lib/routes'))
 app.use(Express.static('./web'))
