6b576af0ecd0b4aa11c74229958f1559aee212cd
Prateek Kejriwal <prateek.kejriwal@glean.com>
2025-05-12 13:04:57 +0530

    Fixed an issue with Backend URL. FIxed SSO

---
+++ web/assets/Spinner-CbXw_LjB.js
@@ -0,0 +1,1 @@
+import{r as i,a as m,j as x,c as $}from"./index-DdTZBDI5.js";const d=i.forwardRef(({bsPrefix:s,variant:r,animation:t="border",size:e,as:n="div",className:o,...p},c)=>{s=m(s,"spinner");const a=`${s}-${t}`;return x.jsx(n,{ref:c,...p,className:$(o,a,e&&`${a}-${e}`,r&&`text-${r}`)})});d.displayName="Spinner";export{d as S};
